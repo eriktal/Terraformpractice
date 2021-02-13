6ece5744afd1b2a39e23a8b798ab65fa7f4fcb13
eric <eriktalla@gmail.com>
2021-02-12 23:02:15 -0600

    Initial commit

---
+++ Webapp/variables.tf
@@ -0,0 +1,4 @@
+variable ami { default = "ami-0a313d6098716f372" }
+variable region { default = "us-east-1" }
+variable project { default = "jenkins" }
+variable mysystem { default = "108.197.92.150/32" }
